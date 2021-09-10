188942884f9358aed39c81c77b5029b7a68317b9
ThanhNguyen <nnct2000@gmail.com>
2021-09-10 20:26:49 +1000

    Add Product Model and Migrations

---
+++ cityfmcodetest/cityfmcodetest/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cityfmcodetest.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int Name { get; set; }
+        public string Description { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int? MaximumQuantity { get; set; }
+    }
+}
